e05ad221049531dcef3009e12473624d098d472b
Tobias Nørbygaard <noerbygaard@hotmail.com>
2023-01-09 15:15:42 +0100

    New test commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
     },
     { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: "e33",
+      id: "e3333",
       title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
